33eaa042ec6160fe7a9b6a5d318e6da4d83945c6
solomongrad <solomongrad@gmail.com>
2024-05-06 12:54:21 +0300

    asd

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -9,6 +9,7 @@ class CreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('blog:index')
 
+
 def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
 
